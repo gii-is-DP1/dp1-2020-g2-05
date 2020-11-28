34eb3d0e9649112db347f5fbc08e75bce51618b7
Miguel Ángel Nieva Arjona <miguenieva2000@gmail.com>
2020-11-28 02:59:53 +0100

    Algunas pruebas de Team

---
+++ src/test/java/org/springframework/samples/petclinic/model/TeamValidatorTests.java
@@ -0,0 +1,40 @@
+package org.springframework.samples.petclinic.model;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Locale;
+import java.util.Set;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
+public class TeamValidatorTests {
+	
+	private Validator createValidator() {
+		LocalValidatorFactoryBean localValidatorFactoryBean = new LocalValidatorFactoryBean();
+		localValidatorFactoryBean.afterPropertiesSet();
+		return localValidatorFactoryBean;
+	}
+
+	@Test
+	void shouldNotValidateWhenNameEmpty() {
+
+		Team team = new Team();
+		team.setName("");
+		
+
+		Validator validator = createValidator();
+		Set<ConstraintViolation<Team>> constraintViolations = validator.validate(team);
+
+		assertThat(constraintViolations.size()).isEqualTo(1);
+		ConstraintViolation<Team> violation = constraintViolations.iterator().next();
+		assertThat(violation.getPropertyPath().toString()).isEqualTo("name");
+		assertThat(violation.getMessage()).isEqualTo("must not be empty");
+	}
+
+
+}
