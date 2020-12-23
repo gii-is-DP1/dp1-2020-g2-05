3e3d69d1671d73e83a1c2390f290c0320f1eb8fe
Mariano Manuel Torrado Sánchez <marianomtorrados@gmail.com>
2020-12-23 16:56:42 +0100

    Gestión de errores personalizada añadida

--- src/main/java/org/springframework/samples/petclinic/configuration/ExceptionHandlerConfiguration.java
+++ src/main/java/org/springframework/samples/petclinic/configuration/ExceptionHandlerConfiguration.java
@@ -4,10 +4,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.web.servlet.error.BasicErrorController;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.ResponseEntity;
+//import org.springframework.samples.petclinic.web.CustomErrorController;
 import org.springframework.validation.BindException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Enumeration;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 import javax.servlet.http.HttpServletRequest;
 
 /**
@@ -19,14 +27,25 @@ import javax.servlet.http.HttpServletRequest;
 public class ExceptionHandlerConfiguration 
 {
 	@Autowired
-	private BasicErrorController errorController;
+//	private CustomErrorController customErrorController;
+	private BasicErrorController basicErrorController;
     // add any exceptions/validations/binding problems
 
    @ExceptionHandler(Exception.class)
    public String defaultErrorHandler(HttpServletRequest request,  Exception ex)  {
+	   
+	   Map<String, Object> atributos= basicErrorController.error(request).getBody();
+       List<String> keys = atributos.keySet().stream().collect(Collectors.toList());
+       
         request.setAttribute("javax.servlet.error.request_uri", request.getPathInfo());
         request.setAttribute("javax.servlet.error.status_code", 400);
-        request.setAttribute("exeption", ex);
-        return "exception";
+        request.setAttribute("exception", ex); //Habia una errata, ponia "exeption"
+        request.setAttribute("timestamp", atributos.get(keys.get(0)));
+        request.setAttribute("error", atributos.get(keys.get(1)));
+        request.setAttribute("status", atributos.get(keys.get(2)));
+        request.setAttribute("message", atributos.get(keys.get(3)));
+        request.setAttribute("trace", atributos.get(keys.get(4)));
+
+        return "error";
     }
 }
