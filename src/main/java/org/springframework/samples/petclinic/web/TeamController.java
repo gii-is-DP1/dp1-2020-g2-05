e1e2c983d75ac17b9179735a7d6c74f4d854949d
Miguel Ángel Nieva Arjona <miguenieva2000@gmail.com>
2021-01-04 01:00:10 +0100

    TeamEditSegunRol

--- src/main/java/org/springframework/samples/petclinic/web/TeamController.java
+++ src/main/java/org/springframework/samples/petclinic/web/TeamController.java
@@ -1,17 +1,9 @@
 package org.springframework.samples.petclinic.web;
 
-import java.util.ArrayList;
-
-
-
 import java.util.Comparator;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-import java.util.Set;
-import java.util.SortedSet;
-import java.util.TreeSet;
 import java.util.stream.Collectors;
 
 import javax.validation.Valid;
@@ -19,21 +11,13 @@ import javax.validation.Valid;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.samples.petclinic.model.League;
-import org.springframework.samples.petclinic.model.Lineup;
 import org.springframework.samples.petclinic.model.Offer;
-import org.springframework.samples.petclinic.model.Pet;
-import org.springframework.samples.petclinic.model.Pilot;
 import org.springframework.samples.petclinic.model.Recruit;
 import org.springframework.samples.petclinic.model.Team;
 import org.springframework.samples.petclinic.model.User;
-
 import org.springframework.samples.petclinic.service.LeagueService;
 import org.springframework.samples.petclinic.service.LineupService;
-
-import org.springframework.samples.petclinic.service.PilotService;
-
 import org.springframework.samples.petclinic.service.OfferService;
-
 import org.springframework.samples.petclinic.service.RecruitService;
 import org.springframework.samples.petclinic.service.UserService;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -46,91 +30,91 @@ import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
-
 @Controller
 public class TeamController {
-	
 
-	
 	UserService userService;
-	
-	
+
 	LeagueService leagueService;
-	
+
 	RecruitService recruitService;
-	
 
 	OfferService offerService;
 
 	LineupService lineupService;
-	
-	
+
 	@Autowired
-	public TeamController(LeagueService leagueService, UserService userService,RecruitService recruitService, LineupService lineupService, OfferService offerService) {
+
+
+	public TeamController(LeagueService leagueService, UserService userService, RecruitService recruitService,
+			LineupService lineupService) {
 		this.leagueService = leagueService;
 		this.userService = userService;
 		this.recruitService = recruitService;
 		this.lineupService = lineupService;	
 		this.offerService = offerService;
+		this.lineupService = lineupService;
 	}
-	
 
-	
 	@InitBinder("team")
 	public void initPetBinder(WebDataBinder dataBinder) {
 		dataBinder.setValidator(new TeamValidator());
 	}
 
+	private String authority;
+	private Boolean EquipoSi = false;
+	private Boolean EquipoNo = false;
+	private Boolean Error = false;
+	private Boolean BorrarDesdeMyTeams = false;
+	private Boolean EditarDesdeMyTeams = false;
 
-	private Boolean EquipoSi=false;
-	private Boolean EquipoNo=false;
-	private Boolean Error=false;
-	private Boolean Editar=false;
-	private Boolean BorrarDesdeMyTeams=false;
-	
 	public User getUserSession() {
-		User usuario = new User();  
+		User usuario = new User();
 		try {
-			  Object auth = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-			  Integer index1 = auth.toString().indexOf("Username:");
-			  Integer index2 = auth.toString().indexOf("; Password:"); // CON ESTO TENEMOS EL STRIN Username: user
-			  String nombreUsuario = auth.toString().substring(index1, index2).split(": ")[1]; //con esto hemos spliteado lo de arriba y nos hemos quedado con user.
+			Object auth = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+			Integer index1 = auth.toString().indexOf("Username:");
+			Integer index2 = auth.toString().indexOf("; Password:"); // CON ESTO TENEMOS EL STRIN Username: user
+			String nombreUsuario = auth.toString().substring(index1, index2).split(": ")[1]; // con esto hemos spliteado
+																								// lo de arriba y nos
+																								// hemos quedado con
+																								// user.
 
-			  Optional<User> user = this.userService.findUser(nombreUsuario);
-			  
-			  usuario =  user.get();
-		  }catch (Exception e) {	
+			Optional<User> user = this.userService.findUser(nombreUsuario);
+
+			usuario = user.get();
+		} catch (Exception e) {
 			// TODO: handle exception
-		  }
+		}
 		return usuario;
 	}
-	
-	
-	@GetMapping(path="/leagues/{leagueId}/teams/new")
+
+	@GetMapping(path = "/leagues/{leagueId}/teams/new")
 	public String crearEquipo(@PathVariable("leagueId") int leagueId, ModelMap model) {
 		Team team = new Team();
 		League liga = this.leagueService.findLeague(leagueId).get();
 		team.setLeague(liga);
 		team.setUser(this.userService.getUserSession());
 		System.out.println(team.getLeague());
-		model.addAttribute("team",team);
-		
-	  return "/leagues/TeamsEdit";
+		model.addAttribute("team", team);
+
+		return "/leagues/TeamsEdit";
 	}
 	
 	
 	//// ESTE ES EL METODO PROFESOR////// ---------->
 	
+
 	@PostMapping(value = "/leagues/{leagueId}/teams/new")
 	public String saveNewTeam(@PathVariable("leagueId") int leagueId, @Valid Team team, BindingResult result, ModelMap model) {
 		League league = this.leagueService.findLeague(leagueId).get();
 		team.setLeague(league);
+//		System.out.println(league.get().getId().equals(team.getLeague().getId()));
 		System.out.println(team.getLeague());
 		System.out.println(team.getUser());
 		System.out.println(result.getAllErrors());
-		if(result.hasErrors()) {
+		if (result.hasErrors()) {
 			model.put("team", team);
-			model.put("message",result.getAllErrors());
+			model.put("message", result.getAllErrors());
 			return "/leagues/TeamsEdit";
 		}else {
 		
@@ -149,124 +133,138 @@ public class TeamController {
 			EquipoSi=true;
 
 			return "redirect:/leagues/{leagueId}/teams";
+			
 			}
 		}
 
 	}
-	
-	@GetMapping(path="/leagues/{leagueId}/teams/{teamId}/details")
-	public String mostrarDetallesEscuderia (@PathVariable("leagueId") int leagueId, @PathVariable("teamId") int teamID,  ModelMap model) {
+
+	@GetMapping(path = "/leagues/{leagueId}/teams/{teamId}/details")
+	public String mostrarDetallesEscuderia(@PathVariable("leagueId") int leagueId, @PathVariable("teamId") int teamID,
+			ModelMap model) {
 		Optional<Team> team = leagueService.findTeamById(teamID);
-		if(team.isPresent()) {
+		if (team.isPresent()) {
 			model.addAttribute("message", "Team found!");
 			model.addAttribute("team", team.get());
 			List<Recruit> l = recruitService.getRecruitsByTeam(teamID);
 			System.out.println(l);
 			model.addAttribute("misFichajes", l);
-			model.addAttribute("misAlineaciones",lineupService.findByTeam(teamID));
-		}else {
+			model.addAttribute("misAlineaciones", lineupService.findByTeam(teamID));
+		} else {
 			model.addAttribute("message", "Team not found!");
 		}
 		return "/leagues/teamDetails";
 	}
-	
-	@GetMapping(path="/leagues/{leagueId}/teams/{teamId}/details/{recruitId}")
+
+	@GetMapping(path = "/leagues/{leagueId}/teams/{teamId}/details/{recruitId}")
 	public String setPrice(@PathVariable("leagueId") int leagueId, @PathVariable("teamId") int teamId,
 			@PathVariable("recruitId") int recruitId, ModelMap modelMap) {
-		
+
 		Optional<Recruit> opRecruit = recruitService.findRecruit(recruitId);
-		if(opRecruit.isPresent()) {
+		if (opRecruit.isPresent()) {
 			modelMap.addAttribute("offer", new Offer());
-			modelMap.addAttribute("recruitToSale",opRecruit.get());
-			return mostrarDetallesEscuderia(leagueId,teamId,modelMap);
-		}else {
+			modelMap.addAttribute("recruitToSale", opRecruit.get());
+			return mostrarDetallesEscuderia(leagueId, teamId, modelMap);
+		} else {
 			modelMap.addAttribute("message", "Recruit not found!");
 			return "/leagues/teamDetails";
 		}
 	}
-	
-	@PostMapping(path="/leagues/{leagueId}/teams/{teamId}/details/{recruitId}")
-	public String putOnSale(@PathVariable("leagueId") int leagueId, @PathVariable("teamId") int teamId, @PathVariable("recruitId") int recruitId,
-			@Valid Offer offer,BindingResult result, ModelMap modelMap) {
-		if(result.hasErrors()) {
+
+	@PostMapping(path = "/leagues/{leagueId}/teams/{teamId}/details/{recruitId}")
+	public String putOnSale(@PathVariable("leagueId") int leagueId, @PathVariable("teamId") int teamId,
+			@PathVariable("recruitId") int recruitId, @Valid Offer offer, BindingResult result, ModelMap modelMap) {
+		if (result.hasErrors()) {
 			System.out.println(result);
-			modelMap.put("message",result.getAllErrors());
-			return setPrice(leagueId,teamId,recruitId,modelMap);
-		}else {
+			modelMap.put("message", result.getAllErrors());
+			return setPrice(leagueId, teamId, recruitId, modelMap);
+		} else {
 			Optional<Recruit> opRecruit = recruitService.findRecruit(recruitId);
-			if(opRecruit.isPresent()) {
+			if (opRecruit.isPresent() && recruitService.getRecruitsByTeam(teamId).size() == 2) {// RN:08 Mínimo de
+																								// fichajes
 				offerService.putOnSale(opRecruit.get(), offer.getPrice());
 				return "redirect:/leagues/{leagueId}/market";
-			}else {
-				modelMap.addAttribute("message", "Recruit not found!");
+			} else {
+				modelMap.addAttribute("message", "Recruit not found or you only own 2 riders!");
 				return "/leagues/teamDetails";
 			}
 		}
 	}
-	
-	@GetMapping(path="/leagues/{leagueId}/teams/{teamId}/delete")
-	public String borrarEscuderia (@PathVariable("leagueId") int leagueId, @PathVariable("teamId") int teamId,  ModelMap model) {
+
+	@GetMapping(path = "/leagues/{leagueId}/teams/{teamId}/delete")
+	public String borrarEscuderia(@PathVariable("leagueId") int leagueId, @PathVariable("teamId") int teamId,
+			ModelMap model) {
 		Optional<Team> team = this.leagueService.findTeamById(teamId);
-		if(team.isPresent()) {
+		if (team.isPresent()) {
 			leagueService.delete(team.get());
 			model.addAttribute("message", "Team successfully deleted!");
-		}else {
+		} else {
 			model.addAttribute("message", "Team not found!");
 		}
-		if(BorrarDesdeMyTeams) {
+		if (BorrarDesdeMyTeams) {
 			BorrarDesdeMyTeams = false;
-			return "redirect:/myTeams"; 
-		}else {
-		return "redirect:/leagues/{leagueId}/teams";
+			return "redirect:/myTeams";
+		} else {
+			return "redirect:/leagues/{leagueId}/teams";
 		}
-		
 
 	}
-	
-	@GetMapping(path="/leagues/{leagueId}/teams/{teamId}/edit")
-	public String editarPiloto(@PathVariable("leagueId") int leagueId, @PathVariable("teamId") int teamId, ModelMap model) {
+
+	@GetMapping(path = "/leagues/{leagueId}/teams/{teamId}/edit")
+	public String editarPiloto(@PathVariable("leagueId") int leagueId, @PathVariable("teamId") int teamId,
+			ModelMap model) {
 		Optional<Team> team = this.leagueService.findTeamById(teamId);
-				model.put("team", team.get());
-				
-				Editar = true;
-				
-				model.put("Editar", Editar);
-				return "leagues/TeamsEdit";
-		
+		model.put("team", team.get());
+		authority = this.leagueService.findAuthoritiesByUsername(team.get().getUser().getUsername());
+
+
+		model.put("Editar", true);
+		if(authority.equals("admin")) {
+			model.put("admin", true);
+		}else {
+			model.put("usuario", true);
+		}
+		return "leagues/TeamsEdit";
+
 	}
-	
+
 	@PostMapping(value = "/leagues/{leagueId}/teams/{teamId}/edit")
-	public String editarPilotoPost(@PathVariable("leagueId") int leagueId, @PathVariable("teamId") int teamId, @Valid Team team, ModelMap model, BindingResult result) {
+	public String editarPilotoPost(@PathVariable("leagueId") int leagueId, @PathVariable("teamId") int teamId,
+			@Valid Team team, ModelMap model, BindingResult result) {
 		System.out.println(result);
 		if (result.hasErrors()) {
 			model.put("team", team);
-			
+
 			return "leagues/TeamsEdit";
-		}
-		else {
+		} else {
 			User usuario = this.userService.getUserSession();
 
-			Editar = false;
 			Team teamToUpdate = this.leagueService.findTeamById(team.getId()).get();
 			BeanUtils.copyProperties(team, teamToUpdate);
 			model.put("team", team);
 			teamToUpdate.setUser(usuario);
 			this.leagueService.saveTeam(teamToUpdate);
-			model.addAttribute("message", "Team successfully saved!");
+			model.addAttribute("message", "Team successfully Updated!");
+			
+			if(BorrarDesdeMyTeams != true) {
 			return "redirect:/leagues/{leagueId}/teams";
+			}else {
+				BorrarDesdeMyTeams =  false;
+				return "redirect:/myTeams";
+
+			}
 		}
 	}
-	
+
 	@GetMapping("/myTeams")
 	public String myTeams(ModelMap modelMap) {
-		User user  =this.userService.getUserSession();
+		User user = this.userService.getUserSession();
 		List<Team> team = this.leagueService.findTeamByUsername(user.getUsername());
-	    modelMap.addAttribute("teams", team);
-	    BorrarDesdeMyTeams = true;
+		modelMap.addAttribute("teams", team);
+		BorrarDesdeMyTeams = true;
 		return "leagues/myTeams";
 	}
 
-	
 	@GetMapping(value = "/leagues/{leagueId}/teams")
 	public String showTeams(@PathVariable int leagueId, Map<String, Object> model) {
 		User usuario = this.userService.getUserSession();
@@ -277,15 +275,19 @@ public class TeamController {
 		model.put("league", this.leagueService.findLeague(leagueId).get());
 		model.put("user", usuario);
 		BorrarDesdeMyTeams = false;
-		if(EquipoNo) model.put("EquipoNo", "You cannot have more than 1 teams in the same league");
-		EquipoNo=false;
-		if(EquipoSi) model.put("EquipoSi", "Team created succesfully!");
-		EquipoSi=false;
-		if(Error) model.put("Error", "Your team have some errors!");
-		Error=false;
-		if(Error) model.put("Error", "Your team have some errors!");
-		Error=false;
-		
+		if (EquipoNo)
+			model.put("EquipoNo", "You cannot have more than 1 teams in the same league");
+		EquipoNo = false;
+		if (EquipoSi)
+			model.put("EquipoSi", "Team created succesfully!");
+		EquipoSi = false;
+		if (Error)
+			model.put("Error", "Your team have some errors!");
+		Error = false;
+		if (Error)
+			model.put("Error", "Your team have some errors!");
+		Error = false;
+
 		return "/leagues/TeamList";
 	}
 }
