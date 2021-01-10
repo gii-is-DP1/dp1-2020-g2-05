23e930d1a31dd8ad87c13c8322bedc43774b97a9
Mariano Manuel Torrado Sánchez <marianomtorrados@gmail.com>
2021-01-10 21:27:19 +0100

    Assertions generadas para el paquete model entero

---
+++ src/test/generated-assertions/org/assertj/BddAssertions.java
@@ -0,0 +1,367 @@
+package org.assertj;
+
+/**
+ * Entry point for BDD assertions of different data types.
+ */
+@javax.annotation.Generated(value="assertj-assertions-generator")
+public class BddAssertions {
+
+  /**
+   * Creates a new instance of <code>{@link org.springframework.samples.petclinic.model.AuthoritiesAssert}</code>.
+   *
+   * @param actual the actual value.
+   * @return the created assertion object.
+   */
+  @org.assertj.core.util.CheckReturnValue
+  public static org.springframework.samples.petclinic.model.AuthoritiesAssert then(org.springframework.samples.petclinic.model.Authorities actual) {
+    return new org.springframework.samples.petclinic.model.AuthoritiesAssert(actual);
+  }
+
+  /**
+   * Creates a new instance of <code>{@link org.springframework.samples.petclinic.model.BDCarreraAssert}</code>.
+   *
+   * @param actual the actual value.
+   * @return the created assertion object.
+   */
+  @org.assertj.core.util.CheckReturnValue
+  public static org.springframework.samples.petclinic.model.BDCarreraAssert then(org.springframework.samples.petclinic.model.BDCarrera actual) {
+    return new org.springframework.samples.petclinic.model.BDCarreraAssert(actual);
+  }
+
+  /**
+   * Creates a new instance of <code>{@link org.springframework.samples.petclinic.model.BaseEntityAssert}</code>.
+   *
+   * @param actual the actual value.
+   * @return the created assertion object.
+   */
+  @org.assertj.core.util.CheckReturnValue
+  public static org.springframework.samples.petclinic.model.BaseEntityAssert then(org.springframework.samples.petclinic.model.BaseEntity actual) {
+    return new org.springframework.samples.petclinic.model.BaseEntityAssert(actual);
+  }
+
+  /**
+   * Creates a new instance of <code>{@link org.springframework.samples.petclinic.model.CategoryAssert}</code>.
+   *
+   * @param actual the actual value.
+   * @return the created assertion object.
+   */
+  @org.assertj.core.util.CheckReturnValue
+  public static org.springframework.samples.petclinic.model.CategoryAssert then(org.springframework.samples.petclinic.model.Category actual) {
+    return new org.springframework.samples.petclinic.model.CategoryAssert(actual);
+  }
+
+  /**
+   * Creates a new instance of <code>{@link org.springframework.samples.petclinic.model.FormRellenarBDAssert}</code>.
+   *
+   * @param actual the actual value.
+   * @return the created assertion object.
+   */
+  @org.assertj.core.util.CheckReturnValue
+  public static org.springframework.samples.petclinic.model.FormRellenarBDAssert then(org.springframework.samples.petclinic.model.FormRellenarBD actual) {
+    return new org.springframework.samples.petclinic.model.FormRellenarBDAssert(actual);
+  }
+
+  /**
+   * Creates a new instance of <code>{@link org.springframework.samples.petclinic.model.GranPremioAssert}</code>.
+   *
+   * @param actual the actual value.
+   * @return the created assertion object.
+   */
+  @org.assertj.core.util.CheckReturnValue
+  public static org.springframework.samples.petclinic.model.GranPremioAssert then(org.springframework.samples.petclinic.model.GranPremio actual) {
+    return new org.springframework.samples.petclinic.model.GranPremioAssert(actual);
+  }
+
+  /**
+   * Creates a new instance of <code>{@link org.springframework.samples.petclinic.model.LeagueAssert}</code>.
+   *
+   * @param actual the actual value.
+   * @return the created assertion object.
+   */
+  @org.assertj.core.util.CheckReturnValue
+  public static org.springframework.samples.petclinic.model.LeagueAssert then(org.springframework.samples.petclinic.model.League actual) {
+    return new org.springframework.samples.petclinic.model.LeagueAssert(actual);
+  }
+
+  /**
+   * Creates a new instance of <code>{@link org.springframework.samples.petclinic.model.LineupAssert}</code>.
+   *
+   * @param actual the actual value.
+   * @return the created assertion object.
+   */
+  @org.assertj.core.util.CheckReturnValue
+  public static org.springframework.samples.petclinic.model.LineupAssert then(org.springframework.samples.petclinic.model.Lineup actual) {
+    return new org.springframework.samples.petclinic.model.LineupAssert(actual);
+  }
+
+  /**
+   * Creates a new instance of <code>{@link org.springframework.samples.petclinic.model.LineupTestsAssert}</code>.
+   *
+   * @param actual the actual value.
+   * @return the created assertion object.
+   */
+  @org.assertj.core.util.CheckReturnValue
+  public static org.springframework.samples.petclinic.model.LineupTestsAssert then(org.springframework.samples.petclinic.model.LineupTests actual) {
+    return new org.springframework.samples.petclinic.model.LineupTestsAssert(actual);
+  }
+
+  /**
+   * Creates a new instance of <code>{@link org.springframework.samples.petclinic.model.MessageAssert}</code>.
+   *
+   * @param actual the actual value.
+   * @return the created assertion object.
+   */
+  @org.assertj.core.util.CheckReturnValue
+  public static org.springframework.samples.petclinic.model.MessageAssert then(org.springframework.samples.petclinic.model.Message actual) {
+    return new org.springframework.samples.petclinic.model.MessageAssert(actual);
+  }
+
+  /**
+   * Creates a new instance of <code>{@link org.springframework.samples.petclinic.model.NamedEntityAssert}</code>.
+   *
+   * @param actual the actual value.
+   * @return the created assertion object.
+   */
+  @org.assertj.core.util.CheckReturnValue
+  public static org.springframework.samples.petclinic.model.NamedEntityAssert then(org.springframework.samples.petclinic.model.NamedEntity actual) {
+    return new org.springframework.samples.petclinic.model.NamedEntityAssert(actual);
+  }
+
+  /**
+   * Creates a new instance of <code>{@link org.springframework.samples.petclinic.model.OfferAssert}</code>.
+   *
+   * @param actual the actual value.
+   * @return the created assertion object.
+   */
+  @org.assertj.core.util.CheckReturnValue
+  public static org.springframework.samples.petclinic.model.OfferAssert then(org.springframework.samples.petclinic.model.Offer actual) {
+    return new org.springframework.samples.petclinic.model.OfferAssert(actual);
+  }
+
+  /**
+   * Creates a new instance of <code>{@link org.springframework.samples.petclinic.model.PersonAssert}</code>.
+   *
+   * @param actual the actual value.
+   * @return the created assertion object.
+   */
+  @org.assertj.core.util.CheckReturnValue
+  public static org.springframework.samples.petclinic.model.PersonAssert then(org.springframework.samples.petclinic.model.Person actual) {
+    return new org.springframework.samples.petclinic.model.PersonAssert(actual);
+  }
+
+  /**
+   * Creates a new instance of <code>{@link org.springframework.samples.petclinic.model.PilotAssert}</code>.
+   *
+   * @param actual the actual value.
+   * @return the created assertion object.
+   */
+  @org.assertj.core.util.CheckReturnValue
+  public static org.springframework.samples.petclinic.model.PilotAssert then(org.springframework.samples.petclinic.model.Pilot actual) {
+    return new org.springframework.samples.petclinic.model.PilotAssert(actual);
+  }
+
+  /**
+   * Creates a new instance of <code>{@link org.springframework.samples.petclinic.model.RecordAssert}</code>.
+   *
+   * @param actual the actual value.
+   * @return the created assertion object.
+   */
+  @org.assertj.core.util.CheckReturnValue
+  public static org.springframework.samples.petclinic.model.RecordAssert then(org.springframework.samples.petclinic.model.Record actual) {
+    return new org.springframework.samples.petclinic.model.RecordAssert(actual);
+  }
+
+  /**
+   * Creates a new instance of <code>{@link org.springframework.samples.petclinic.model.RecordCircuitoAssert}</code>.
+   *
+   * @param actual the actual value.
+   * @return the created assertion object.
+   */
+  @org.assertj.core.util.CheckReturnValue
+  public static org.springframework.samples.petclinic.model.RecordCircuitoAssert then(org.springframework.samples.petclinic.model.RecordCircuito actual) {
+    return new org.springframework.samples.petclinic.model.RecordCircuitoAssert(actual);
+  }
+
+  /**
+   * Creates a new instance of <code>{@link org.springframework.samples.petclinic.model.RecordMejorVueltaAssert}</code>.
+   *
+   * @param actual the actual value.
+   * @return the created assertion object.
+   */
+  @org.assertj.core.util.CheckReturnValue
+  public static org.springframework.samples.petclinic.model.RecordMejorVueltaAssert then(org.springframework.samples.petclinic.model.RecordMejorVuelta actual) {
+    return new org.springframework.samples.petclinic.model.RecordMejorVueltaAssert(actual);
+  }
+
+  /**
+   * Creates a new instance of <code>{@link org.springframework.samples.petclinic.model.RecordPoleAssert}</code>.
+   *
+   * @param actual the actual value.
+   * @return the created assertion object.
+   */
+  @org.assertj.core.util.CheckReturnValue
+  public static org.springframework.samples.petclinic.model.RecordPoleAssert then(org.springframework.samples.petclinic.model.RecordPole actual) {
+    return new org.springframework.samples.petclinic.model.RecordPoleAssert(actual);
+  }
+
+  /**
+   * Creates a new instance of <code>{@link org.springframework.samples.petclinic.model.RecordVueltaRapidaAssert}</code>.
+   *
+   * @param actual the actual value.
+   * @return the created assertion object.
+   */
+  @org.assertj.core.util.CheckReturnValue
+  public static org.springframework.samples.petclinic.model.RecordVueltaRapidaAssert then(org.springframework.samples.petclinic.model.RecordVueltaRapida actual) {
+    return new org.springframework.samples.petclinic.model.RecordVueltaRapidaAssert(actual);
+  }
+
+  /**
+   * Creates a new instance of <code>{@link org.springframework.samples.petclinic.model.RecruitAssert}</code>.
+   *
+   * @param actual the actual value.
+   * @return the created assertion object.
+   */
+  @org.assertj.core.util.CheckReturnValue
+  public static org.springframework.samples.petclinic.model.RecruitAssert then(org.springframework.samples.petclinic.model.Recruit actual) {
+    return new org.springframework.samples.petclinic.model.RecruitAssert(actual);
+  }
+
+  /**
+   * Creates a new instance of <code>{@link org.springframework.samples.petclinic.model.ResultAssert}</code>.
+   *
+   * @param actual the actual value.
+   * @return the created assertion object.
+   */
+  @org.assertj.core.util.CheckReturnValue
+  public static org.springframework.samples.petclinic.model.ResultAssert then(org.springframework.samples.petclinic.model.Result actual) {
+    return new org.springframework.samples.petclinic.model.ResultAssert(actual);
+  }
+
+  /**
+   * Creates a new instance of <code>{@link org.springframework.samples.petclinic.model.RolAssert}</code>.
+   *
+   * @param actual the actual value.
+   * @return the created assertion object.
+   */
+  @org.assertj.core.util.CheckReturnValue
+  public static org.springframework.samples.petclinic.model.RolAssert then(org.springframework.samples.petclinic.model.Rol actual) {
+    return new org.springframework.samples.petclinic.model.RolAssert(actual);
+  }
+
+  /**
+   * Creates a new instance of <code>{@link org.springframework.samples.petclinic.model.StatusAssert}</code>.
+   *
+   * @param actual the actual value.
+   * @return the created assertion object.
+   */
+  @org.assertj.core.util.CheckReturnValue
+  public static org.springframework.samples.petclinic.model.StatusAssert then(org.springframework.samples.petclinic.model.Status actual) {
+    return new org.springframework.samples.petclinic.model.StatusAssert(actual);
+  }
+
+  /**
+   * Creates a new instance of <code>{@link org.springframework.samples.petclinic.model.TablaConsultasAssert}</code>.
+   *
+   * @param actual the actual value.
+   * @return the created assertion object.
+   */
+  @org.assertj.core.util.CheckReturnValue
+  public static org.springframework.samples.petclinic.model.TablaConsultasAssert then(org.springframework.samples.petclinic.model.TablaConsultas actual) {
+    return new org.springframework.samples.petclinic.model.TablaConsultasAssert(actual);
+  }
+
+  /**
+   * Creates a new instance of <code>{@link org.springframework.samples.petclinic.model.TeamAssert}</code>.
+   *
+   * @param actual the actual value.
+   * @return the created assertion object.
+   */
+  @org.assertj.core.util.CheckReturnValue
+  public static org.springframework.samples.petclinic.model.TeamAssert then(org.springframework.samples.petclinic.model.Team actual) {
+    return new org.springframework.samples.petclinic.model.TeamAssert(actual);
+  }
+
+  /**
+   * Creates a new instance of <code>{@link org.springframework.samples.petclinic.model.TeamModelTestAssert}</code>.
+   *
+   * @param actual the actual value.
+   * @return the created assertion object.
+   */
+  @org.assertj.core.util.CheckReturnValue
+  public static org.springframework.samples.petclinic.model.TeamModelTestAssert then(org.springframework.samples.petclinic.model.TeamModelTest actual) {
+    return new org.springframework.samples.petclinic.model.TeamModelTestAssert(actual);
+  }
+
+  /**
+   * Creates a new instance of <code>{@link org.springframework.samples.petclinic.model.TeamValidatorTestsAssert}</code>.
+   *
+   * @param actual the actual value.
+   * @return the created assertion object.
+   */
+  @org.assertj.core.util.CheckReturnValue
+  public static org.springframework.samples.petclinic.model.TeamValidatorTestsAssert then(org.springframework.samples.petclinic.model.TeamValidatorTests actual) {
+    return new org.springframework.samples.petclinic.model.TeamValidatorTestsAssert(actual);
+  }
+
+  /**
+   * Creates a new instance of <code>{@link org.springframework.samples.petclinic.model.TransactionAssert}</code>.
+   *
+   * @param actual the actual value.
+   * @return the created assertion object.
+   */
+  @org.assertj.core.util.CheckReturnValue
+  public static org.springframework.samples.petclinic.model.TransactionAssert then(org.springframework.samples.petclinic.model.Transaction actual) {
+    return new org.springframework.samples.petclinic.model.TransactionAssert(actual);
+  }
+
+  /**
+   * Creates a new instance of <code>{@link org.springframework.samples.petclinic.model.TransactionTypeAssert}</code>.
+   *
+   * @param actual the actual value.
+   * @return the created assertion object.
+   */
+  @org.assertj.core.util.CheckReturnValue
+  public static org.springframework.samples.petclinic.model.TransactionTypeAssert then(org.springframework.samples.petclinic.model.TransactionType actual) {
+    return new org.springframework.samples.petclinic.model.TransactionTypeAssert(actual);
+  }
+
+  /**
+   * Creates a new instance of <code>{@link org.springframework.samples.petclinic.model.UserAssert}</code>.
+   *
+   * @param actual the actual value.
+   * @return the created assertion object.
+   */
+  @org.assertj.core.util.CheckReturnValue
+  public static org.springframework.samples.petclinic.model.UserAssert then(org.springframework.samples.petclinic.model.User actual) {
+    return new org.springframework.samples.petclinic.model.UserAssert(actual);
+  }
+
+  /**
+   * Creates a new instance of <code>{@link org.springframework.samples.petclinic.model.audit.UserRevEntityAssert}</code>.
+   *
+   * @param actual the actual value.
+   * @return the created assertion object.
+   */
+  @org.assertj.core.util.CheckReturnValue
+  public static org.springframework.samples.petclinic.model.audit.UserRevEntityAssert then(org.springframework.samples.petclinic.model.audit.UserRevEntity actual) {
+    return new org.springframework.samples.petclinic.model.audit.UserRevEntityAssert(actual);
+  }
+
+  /**
+   * Creates a new instance of <code>{@link org.springframework.samples.petclinic.model.audit.UserRevisionListenerAssert}</code>.
+   *
+   * @param actual the actual value.
+   * @return the created assertion object.
+   */
+  @org.assertj.core.util.CheckReturnValue
+  public static org.springframework.samples.petclinic.model.audit.UserRevisionListenerAssert then(org.springframework.samples.petclinic.model.audit.UserRevisionListener actual) {
+    return new org.springframework.samples.petclinic.model.audit.UserRevisionListenerAssert(actual);
+  }
+
+  /**
+   * Creates a new <code>{@link BddAssertions}</code>.
+   */
+  protected BddAssertions() {
+    // empty
+  }
+}
