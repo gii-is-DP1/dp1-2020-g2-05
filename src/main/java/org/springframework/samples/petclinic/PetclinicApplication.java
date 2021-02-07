e4793745a6984469d4be1a5a1a67d604b6f7b0f6
Negis <Negis@DESKTOP-TP4EH57>
2021-02-07 17:25:43 +0100

    rest template implementado

--- src/main/java/org/springframework/samples/petclinic/PetclinicApplication.java
+++ src/main/java/org/springframework/samples/petclinic/PetclinicApplication.java
@@ -4,11 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.samples.petclinic.service.PoblarBaseDeDatosService;
 import org.springframework.samples.petclinic.service.TablaConsultasService;
 import org.springframework.samples.petclinic.service.TransactionService;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.web.client.RestTemplate;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -51,6 +54,11 @@ public class PetclinicApplication {
 		this.PBDService.poblandoUltimaCarreraCompletada();
 		
     }
+	
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
 	
 
 
