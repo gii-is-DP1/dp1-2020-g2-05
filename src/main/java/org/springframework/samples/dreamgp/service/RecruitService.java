d12c0aa3c0e2acd730b141416e3d4e23f93cd2d3
Alvaro <alvarocortescasado@gmail.com>
2021-02-09 12:50:08 +0100

    Retocando metodos y comentando el funcionamiento

--- src/main/java/org/springframework/samples/dreamgp/service/RecruitService.java
+++ src/main/java/org/springframework/samples/dreamgp/service/RecruitService.java
@@ -50,45 +50,49 @@ public class RecruitService {
 	// Operaciones de Read
 
 	@Transactional(readOnly = true)
-	public List<Recruit> findAll() {
+	public List<Recruit> findAll() throws DataAccessException {
 		return (List<Recruit>) this.recruitRepository.findAll();
 	}
 
 	@Transactional(readOnly = true)
-	public Optional<Recruit> findRecruitById(Integer recruitId) { // Encuentra un fichaje en base a su ID de fichaje
+	public Optional<Recruit> findRecruitById(Integer recruitId) throws DataAccessException {
 		return recruitRepository.findById(recruitId);
 	}
 
 	@Transactional(readOnly = true)
 	public Optional<Recruit> getRecruitByPilotId(int pilotId, int leagueId) throws DataAccessException {
+		// Nos permite ver si existe un fichaje correspondiente a un piloto con id ==
+		// pilotId en la liga con id == leagueId
 		return this.recruitRepository.findRecruitByPilotId(pilotId, leagueId);
 	}
 
 	@Transactional(readOnly = true)
-	public List<Pilot> getRecruits() throws DataAccessException { // Nos da todos los pilotos correspondientes a los
-																	// fichajes que existen en el sistema
+	public List<Pilot> getRecruits() throws DataAccessException {
 		return this.pilotService.getRecruits();
 	}
-	
+
 	@Transactional(readOnly = true)
-	public List<Recruit> getRecruitsByTeam(int teamID) {
+	public List<Recruit> getRecruitsByTeam(int teamID) throws DataAccessException {
 		return this.recruitRepository.findAllRecruitsByTeam(teamID);
 	}
 
 	@Transactional(readOnly = true)
-	public List<Recruit> getRecruitsOnSaleByTeam(int teamID) {
+	public List<Recruit> getRecruitsOnSaleByTeam(int teamID) throws DataAccessException {
 		return this.recruitRepository.findAllRecruitsOnSaleByTeam(teamID);
 	}
 
 	@Transactional(readOnly = true)
-	public List<Recruit> getRecruitsNotOnSaleByTeam(int teamID) {
+	public List<Recruit> getRecruitsNotOnSaleByTeam(int teamID) throws DataAccessException {
 		return this.recruitRepository.findAllRecruitsNotOnSaleByTeam(teamID);
 	}
 
 	// Operaciones de Update
 
 	@Transactional(rollbackFor = NotAllowedNumberOfRecruitsException.class)
-	public boolean putOnSale(Recruit recruit) throws NotAllowedNumberOfRecruitsException {
+	public boolean putOnSale(Recruit recruit) throws DataAccessException, NotAllowedNumberOfRecruitsException {
+		// Gestión de la regla de negocio R6 que exige que el minimo número de pilotos
+		// que un equipo debe tener en posesion es de dos. Por lo cual, al menos dos
+		// fichajes deben estar fuera del mercado
 		if (getRecruitsNotOnSaleByTeam(recruit.getTeam().getId()).size() == 2) {
 			throw new NotAllowedNumberOfRecruitsException();
 		} else {
@@ -99,7 +103,7 @@ public class RecruitService {
 	}
 
 	@Transactional
-	public void quitOnSale(Recruit recruit) {
+	public void quitOnSale(Recruit recruit) throws DataAccessException {
 		recruit.setForSale(false);
 		this.recruitRepository.save(recruit);
 	}
@@ -107,6 +111,9 @@ public class RecruitService {
 	@Transactional(rollbackFor = NotAllowedNumberOfRecruitsException.class)
 	public Boolean purchaseRecruit(Recruit recruit, Team purchaserTeam)
 			throws DataAccessException, NotAllowedNumberOfRecruitsException {
+		// Gestión de la regla de negocio R7 que exige que el máximo número de pilotos
+		// que un equipo debe tener en posesion es de cuatro. Por lo cual, al intentar
+		// realizar el traspaso de un quinto debe elevarse una excepción
 		if (this.getRecruitsByTeam(purchaserTeam.getId()).size() == 4) {
 			throw new NotAllowedNumberOfRecruitsException();
 		} else {
@@ -120,7 +127,7 @@ public class RecruitService {
 	// Operaciones de Delete
 
 	@Transactional
-	public void deleteRecruit(Recruit recruit) {
+	public void deleteRecruit(Recruit recruit) throws DataAccessException {
 		this.recruitRepository.delete(recruit);
 	}
 
